3bd6f0ec6a545feab2f8bf3e073a39d8874763d6
Yash Arora <yasharora102@gmail.com>
2023-12-22 05:57:22 +0000

    change app.py

--- app.py
+++ app.py
@@ -81,4 +81,4 @@ async def upload_image(file: UploadFile, request: Request):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
